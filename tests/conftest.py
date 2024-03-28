d723c68294e312e6cd3c246be1b35f46dad8be8a
afikm <afikmark@gmail.com>
2024-03-28 15:37:31 +0200

    changed localhost url

--- tests/conftest.py
+++ tests/conftest.py
@@ -92,7 +92,7 @@ def pytest_addoption(parser):
     parser.addoption("--is_local", action="store", help="run locally or remotely, accept true/false", default=False)
     parser.addoption("--allurdir", action="store", help="allure results directory", default="allure-results")
     parser.addoption("--remote_mode", action="store", help="healenium OR selenoid",
-                     default=RemoteMode.HEALENIUM)
+                     default=RemoteMode.SELENOID)
 
 
 @pytest.fixture(scope="session")
