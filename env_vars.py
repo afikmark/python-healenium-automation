d7d22b1109e1d276f4bc913ae38413f3f529d949
afikm <afikmark@gmail.com>
2024-04-15 01:31:21 +0300

    secret

--- env_vars.py
+++ env_vars.py
@@ -1,3 +1,3 @@
 from os import environ
 
-MONGO_URL = environ.get("MONGO_URL", "mongodb+srv://afikmark:5JyM1AdyT8zFkvTY@cluster0.ywacopc.mongodb.net/?retryWrites\\\=true&w\\\=majority&appName\\\=Cluster0")
+MONGO_URL = environ.get("MONGO_URL", "")
